9085816f50a03591a80b52c50260ed5e42b07bef
nosorae <nosorae@naver.com>
2021-01-04 00:36:27 +0900

    카카오 로그인과 ui틀 25% (로그인 액티비티에 뷰페이저 구현, 네이버, 페이스북, 전화번호로그인 구현 예정) +아이콘 변경

--- bunjang_a_mock_android_noah/app/src/main/java/com/nosorae/bunjang_a_mock_android_noah/config/ApplicationClass.kt
+++ bunjang_a_mock_android_noah/app/src/main/java/com/nosorae/bunjang_a_mock_android_noah/config/ApplicationClass.kt
@@ -3,6 +3,7 @@ package com.nosorae.bunjang_a_mock_android_noah.config
 import android.app.Application
 import android.content.SharedPreferences
 import android.util.Log
+import com.kakao.sdk.common.KakaoSdk
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -38,6 +39,8 @@ class ApplicationClass : Application() {
             applicationContext.getSharedPreferences("SOFTSQUARED_TEMPLATE_APP", MODE_PRIVATE)
         // 레트로핏 인스턴스 생성
         initRetrofitInstance()
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "3d81bd53b0ed2371312ccff47725aea2")
     }
 
     // 레트로핏 인스턴스를 생성하고, 레트로핏에 각종 설정값들을 지정해줍니다.
--- bunjang_a_mock_android_noah/app/src/main/java/com/nosorae/bunjang_a_mock_android_noah/src/log_in/LogInActivity.kt
+++ bunjang_a_mock_android_noah/app/src/main/java/com/nosorae/bunjang_a_mock_android_noah/src/log_in/LogInActivity.kt
@@ -2,6 +2,11 @@ package com.nosorae.bunjang_a_mock_android_noah.src.log_in
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import com.kakao.sdk.auth.LoginClient
+import com.kakao.sdk.auth.model.AuthType
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.util.Utility
 import com.nosorae.bunjang_a_mock_android_noah.config.BaseActivity
 import com.nosorae.bunjang_a_mock_android_noah.databinding.ActivityLogInBinding
 import com.nosorae.bunjang_a_mock_android_noah.src.main.MainActivity
@@ -11,11 +16,27 @@ class LogInActivity : BaseActivity<ActivityLogInBinding>(ActivityLogInBinding::i
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.logIn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+        binding.imageButtonLogInKakao.setOnClickListener {
+            // 카카오톡으로 로그인
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("kakao", "로그인 실패", error)
+                    showCustomToast("로그인 실패 ${error}")
+                }
+                else if (token != null) {
+                    Log.i("kakao", "로그인 성공 ${token.accessToken}")
+                    showCustomToast("로그인 성공 ${token.accessToken}")
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+
+                }
+            }
+
+            if (LoginClient.instance.isKakaoTalkLoginAvailable(this)) {
+                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
+            } else {
+                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
         }
-
-
     }
 }
